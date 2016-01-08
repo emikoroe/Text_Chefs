84db49c7b55c31a3199a231f665bd8399d5ec3b7
HAVELKA <JHAVELKA@HANOVER.COM>
2016-01-08 09:44:54 -0500

    Added last model that uses random search

--- Cooking_R_Code.R
+++ Cooking_R_Code.R
@@ -6,8 +6,11 @@ library(SnowballC)
 library(Matrix)
 library(caret)
 library(e1071)
+library(foreach)
+library(doParallel)
+library(randomForest)
 
-set.seed(1234)
+set.seed(123456)
 
 ######pull in data
 setwd("~/Kaggle/Text_Chefs/Cooking")
@@ -27,11 +30,9 @@ c_ingredients <- Corpus(VectorSource(dat$ingredients))
 #######
 
 #######create simple DTM
-c_ingredientsDTM <- DocumentTermMatrix(c_ingredients)
+c_ingredientsDTM <- DocumentTermMatrix(c_ingredients2)
 c_ingredientsDTM <- removeSparseTerms(c_ingredientsDTM, 1-3/nrow(c_ingredientsDTM))
 c_ingredientsDTM <- as.data.frame(as.matrix(c_ingredientsDTM))
-#need to take sign to not double count single and multi-word
-c_ingredientsDTM <- sign(c_ingredientsDTM)
 #######
 
 #######feature engineering (basic)
@@ -42,86 +43,77 @@ c_ingredientsDTM_Submit <- c_ingredientsDTM[-(1:nrow(train)),]
 #run this to create sample for testing
 #selected<-sample(nrow(c_ingredientsDTM_Model),25000) 
 #c_ingredientsDTM_Model <- c_ingredientsDTM_Model[selected,]
-#c_ingredientsDTM_Test <- c_ingredientsDTM_Model[-selected,]
+#c_ingredientsDTM_Test <- (c_ingredientsDTM[1:nrow(train),])[-selected,]
 #######
 
-#matrix for models
-xgbmat <- xgb.DMatrix(Matrix(data.matrix(c_ingredientsDTM_Model[, !colnames(c_ingredientsDTM_Model) %in% c("cuisine")])), label=as.numeric(c_ingredientsDTM_Model$cuisine)-1)
-
-
-#######Run these for model tunning.  Don't need to run each time.
-cv.ctrl <- trainControl(method = "repeatedcv", repeats = 1,number = 3, 
-                        #summaryFunction = twoClassSummary,
-                        classProbs = TRUE,
-                        allowParallel=T)
-
-xgb.grid <- expand.grid(nrounds = 1000,
-                        eta = c(0.05,0.1,0.2,0.3),
-                        max_depth = c(10,15,20,25,30)
-)
-xgb_tune <-train(cuisine~.,
-                 data=c_ingredientsDTM_Model,
-                 method="xgbTree",
-                 trControl=cv.ctrl,
-                 tuneGrid=xgb.grid,
-                 verbose=T,
-                 metric="Kappa",
-                 nthread =7
-              )
-xgb_tune$bestTune
-#max_depth 10 eta .05
-
-xgb.grid <- expand.grid(nrounds = 1000,
-                        eta = c(0.01,0.03,0.05,0.07),
-                        max_depth = c(4,6,8,10,12)
-)
-xgb_tune <-train(cuisine~.,
-                 data=c_ingredientsDTM_Model,
-                 method="xgbTree",
-                 trControl=cv.ctrl,
-                 tuneGrid=xgb.grid,
-                 verbose=T,
-                 metric="Kappa",
-                 nthread =7
-)
-xgb_tune$bestTune
-#max_depth 4 eta .07
-
-xgb.grid <- expand.grid(nrounds = 1500,
-                        eta = c(0.06,0.07,0.08,0.09),
-                        max_depth = c(2,3,4,5)
-)
-xgb_tune <-train(cuisine~.,
-                 data=c_ingredientsDTM_Model,
-                 method="xgbTree",
-                 trControl=cv.ctrl,
-                 tuneGrid=xgb.grid,
-                 verbose=T,
-                 metric="Kappa",
-                 nthread =7
-)
-xgb_tune$bestTune
-#max_depth 5 eta .09
-
-xgb.grid <- expand.grid(nrounds = c(500,750,1000,1500,2000,2500),
-                        eta = c(0.09),
-                        max_depth = c(5)
-)
-xgb_tune <-train(cuisine~.,
-                 data=c_ingredientsDTM_Model,
-                 method="xgbTree",
-                 trControl=cv.ctrl,
-                 tuneGrid=xgb.grid,
-                 verbose=T,
-                 metric="Kappa",
-                 nthread =1
-)
-xgb_tune$bestTune
-#nrounds 1500.  Submission on full data got better with 2000.
+#######creating ranges to test for random hyperparameter search
+#x <- c("max.depth",3,7,0,"eta",.05,.2,3,"subsample",.7,.99,3,"colsample_bytree",.1,.5,3)
+#x <- c("max.depth",3,7,0,"eta",.02,.07,3,"subsample",.7,.99,3,"colsample_bytree",.1,.5,3)
+#x <- c("max.depth",6,9,0,"eta",.02,.04,3,"subsample",.85,.99,3,"colsample_bytree",.1,.5,3)
+x <- c("max.depth",6,8,0,"eta",.025,.035,3,"subsample",.95,.999,3,"colsample_bytree",.05,.3,3)
 #######
+
+#######function that creates a list of hyperparameters to test
+randParams <- function(x,n){
+  y <- data.frame(matrix(x,nrow=4),stringsAsFactors=F)
+  names(y) <- y[1,]
+  y <- y[-1,]
+  y <- data.frame(sapply(y,FUN=as.numeric))
+  z <- data.frame(sapply(y,FUN=function(f) {round(runif(n,min=f[1],max=f[2]),f[3])}))
+  return(apply(z,1,list))
+}
+#######
+
+#######setting up a cross validation data set
+samp <- sample(c(1,2,3),nrow(c_ingredientsDTM_Model),replace=T)
+xs <- Matrix(data.matrix(c_ingredientsDTM_Model[, !colnames(c_ingredientsDTM_Model) %in% c("cuisine")]),sparse = T)
+
+xs_train <- list()
+xs_pred <- list()
+for(j in 1:3){
+  xs_train[j] <- Matrix(data.matrix(c_ingredientsDTM_Model[samp!=j, !colnames(c_ingredientsDTM_Model) %in% c("cuisine")]),sparse = T)
+  xs_pred[j] <- Matrix(data.matrix(c_ingredientsDTM_Model[samp==j, !colnames(c_ingredientsDTM_Model) %in% c("cuisine")]),sparse = T)
+}
+ys <- as.numeric(c_ingredientsDTM_Model$cuisine)-1
+#######
+# c_ingredientsDTM <- NULL
+# c_ingredientsDTM_Model <- NULL
+# c_ingredientsDTM_Submit <- NULL
+# dat <- NULL
+# train <- NULL
+# test <- NULL
+# c_ingredients <- NULL
+# gc()
+# 
+# cl <- makeCluster(7)-
+# registerDoParallel(cl)
+
+#######loop that tests different hyperparameters. outputs results to text file as it loops for monitoring. WARNING, this takes a couple of days to run
+t1 <- Sys.time()
+xgb_results <- foreach(i=randParams(x,100),.combine = rbind) %do% {
+  totCorrect <- 0
+  nr <- round(runif(1,3000,5000),0)
+  for(j in 1:3){
+    xgbmat <- xgb.DMatrix(xs_train[[j]], label=ys[samp!=j])
+    xgb <- xgboost(xgbmat, nround = nr, objective = "multi:softmax", num_class = 20,verbose = 0, params = i[[1]])
+    pred <- predict(xgb, newdata = xs_pred[[j]])
+    totCorrect <- totCorrect + sum(pred==ys[samp==j])
+  }
+  line <- c(i[[1]],nround = nr,score=totCorrect/length(ys),elapsed_time=(Sys.time()-t1))
+  print(line)
+  write(line,file="xgb_results.csv",append=TRUE, sep=",",ncolumns=length(line))
+  flush.console()
+  line
+}
+t2 <- Sys.time()
+t2-t1
+#######
+
+# stopCluster(cl)
 
 #######Run Model (run on full data for submission)
-xgb <- xgboost(xgbmat, max.depth = 5, eta = 0.09, nround = 2000, objective = "multi:softmax", num_class = 20)
+xgbmat <- xgb.DMatrix(xs, label=ys)
+xgb <- xgboost(xgbmat, nround = 3870, objective = "multi:softmax", num_class = 20,verbose = 1, max.depth = 7, eta = .029, subsample=.99,colsample_bytree=.154)
 #######
 
 #######Prediction Testing
@@ -132,8 +124,7 @@ summary(pred_class)
 sum(as.character(pred_class)==as.character(c_ingredientsDTM_Test$cuisine))/nrow(c_ingredientsDTM_Test)
 
 table(as.character(pred_class),as.character(c_ingredientsDTM_Test$cuisine))
-c_ingredientsDTM_Test[pred_class=="vietnamese" & c_ingredientsDTM_Test$cuisine=="thai",]
-train[1454,]
+train[rownames(c_ingredientsDTM_Test[pred_class=="italian" & c_ingredientsDTM_Test$cuisine=="japanese",]),]
 #######
 
 #######Create predictions for Submission
@@ -146,4 +137,4 @@ pred_class <- as.factor(pred_class)
 sub <- data.frame(id=test$id,cuisine=pred_class)
 summary(sub)
 
-write.csv(sub,"sub5",row.names=F)
+write.csv(sub,"sub",row.names=F)
